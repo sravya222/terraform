5800a29b86fd27af3ca4f02e47ffdd738b5253d5
sravya222 <97881527+sravya222@users.noreply.github.com>
2025-06-20 17:07:10 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,10 +11,5 @@ terraform {
 provider "aws" {
   region = "eu-central-1"
 }
-provider "aws" {
- % export AWS_ACCESS_KEY_ID="anaccesskey"
-% export AWS_SECRET_ACCESS_KEY="asecretkey"
-% export AWS_REGION="us-west-2"
-% terraform plan
-}
+
 
